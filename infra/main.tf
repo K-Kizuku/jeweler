005f1800f034860e6b389d840586ac0d152043d0
K-Kizuku <kotani.kizuku@gmail.com>
2024-09-02 00:19:50 +0900

    feat: 🍻 chore

--- infra/main.tf
+++ infra/main.tf
@@ -161,9 +161,9 @@ resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.lambda_function.function_name
-  principal     = "*"
-  # principal     = "apigateway.amazonaws.com"
-  # source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+  # principal     = "*"
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
 
 # CloudFrontディストリビューションの作成
--- supabase/functions/jeweler/components.tsx
+++ supabase/functions/jeweler/components.tsx
@@ -18,60 +18,60 @@ export const renderer = jsxRenderer(({ children }) => {
       <title>Jeweler</title>
     </head>
     <body>
-      <div class="p-4">
-        <h1 class="text-4xl font-bold mb-4"><a href="/">Todo</a></h1>
-        ${children}
-      </div>
+      <div class="p-4">${children}</div>
     </body>
   `;
 });
 
 export const AddTodo = () => (
-  <form
-    hx-post="/topaz"
-    hx-target="#topaz"
-    hx-swap="beforebegin"
-    _="on htmx:afterRequest reset() me"
-    class="mb-6 bg-white shadow-md rounded-lg p-6"
-  >
-    <div class="mb-4">
-      <label for="url" class="block text-gray-700 text-sm font-bold mb-2">
-        URL
-      </label>
-      <input
-        id="url"
-        name="url"
-        type="text"
-        placeholder="Enter URL"
-        class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg w-full p-2.5 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        required
-      />
-    </div>
-
-    <div class="mb-4">
-      <label
-        for="product_name"
-        class="block text-gray-700 text-sm font-bold mb-2"
-      >
-        プロダクト名
-      </label>
-      <input
-        id="product_name"
-        name="product_name"
-        type="text"
-        placeholder="Enter Product Name"
-        class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg w-full p-2.5 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        required
-      />
-    </div>
-
-    <button
-      class="w-full text-white bg-blue-700 hover:bg-blue-800 rounded-lg px-5 py-2 text-center font-medium focus:outline-none focus:ring-2 focus:ring-blue-500"
-      type="submit"
+  <div>
+    <img src="https://kizuku-storage.work/jeweler-logo.png" />
+    <form
+      hx-post="/topaz"
+      hx-target="#topaz"
+      hx-swap="beforebegin"
+      _="on htmx:afterRequest reset() me"
+      class="mb-6 bg-white shadow-md rounded-lg p-6"
     >
-      Submit
-    </button>
-  </form>
+      <div class="mb-4">
+        <label for="url" class="block text-gray-700 text-sm font-bold mb-2">
+          URL
+        </label>
+        <input
+          id="url"
+          name="url"
+          type="text"
+          placeholder="Enter URL"
+          class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg w-full p-2.5 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+      </div>
+
+      <div class="mb-4">
+        <label
+          for="product_name"
+          class="block text-gray-700 text-sm font-bold mb-2"
+        >
+          Product Name
+        </label>
+        <input
+          id="product_name"
+          name="product_name"
+          type="text"
+          placeholder="Enter Product Name"
+          class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg w-full p-2.5 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
+      </div>
+
+      <button
+        class="w-full text-white bg-blue-700 hover:bg-blue-800 rounded-lg px-5 py-2 text-center font-medium focus:outline-none focus:ring-2 focus:ring-blue-500"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+  </div>
 );
 
 export const Item = ({ title, id }: { title: string; id: string }) => (
